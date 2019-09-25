6270cb8e35e4147e4b5f5e5cbda67b978371ade7
mac fyfe <macspersonalemail@gmail.com>
2019-09-25 11:20:55 -0500

    hw4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome', to: 'static_pages#welcome', as: 'welcome'
   get 'about', to: 'static_pages#about', as: 'about'
+  get 'contact', to: 'static_pages#contact', as: 'contact'
+  post 'contact', to: 'static_pages#leave_feedback', as: 'leave_feedback'
   root to: redirect('/welcome', status: 302)
 
   #the "as: welcome" part gives us a varPath so we dont have to hardcode anything, nice!
